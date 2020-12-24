de52d929d6007645384f031b471a273d37cbdb77
Dmitry <dmkhost@gmail.com>
2020-12-24 18:05:02 +0200

    add computed properties and  variants.quantity

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
 	data() {
 		return {
 			cart: 0,
+			brand: 'Dim Kovy',
 			product: 'Socks',
 			image: './img/socks_green.jpg',
 			description: 'Socks image',
@@ -10,8 +11,8 @@ const app = Vue.createApp({
 			onSale: true,
 			details: ['50% cotton', '30% wool', '20% polyester'],
 			variants: [
-				{ id: 2234, color: 'green', image: './img/socks_green.jpg'},
-				{ id: 2235, color: 'blue', image: './img/socks_blue.jpg'},
+				{ id: 2234, color: 'green', image: './img/socks_green.jpg', quantity: 50 },
+				{ id: 2235, color: 'blue', image: './img/socks_blue.jpg', quantity: 0 },
 			],
 			sizes: ['Shoe size: 5-9', 'Shoe size: 6-12', 'Shoe size: 12-15', 'Shoe size: 15-17'],
 			
@@ -27,6 +28,11 @@ const app = Vue.createApp({
 		},
 		updateImage(variantImage) {
 			this.image = variantImage
+		}
+	},
+	computed: {
+		title() {
+			return this.brand + ' ' + this.product
 		}
 	}
 })
