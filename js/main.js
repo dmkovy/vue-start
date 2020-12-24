7d0b5f27ebb92757846e0e986a90cc5d2dc36884
Dmitry <dmkhost@gmail.com>
2020-12-24 14:35:18 +0200

    update data image path

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
 	data() {
 		return {
 			product: 'Socks',
-			image: '../img/socks_green.jpg',
+			image: './img/socks_green.jpg',
 			description: 'Socks image',
 			// inStock: true,
 			inventory: 100,
