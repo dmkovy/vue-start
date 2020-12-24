e04505ff7bc41a9985571f41a3608ade71491467
Dmitry <dmkhost@gmail.com>
2020-12-24 14:30:38 +0200

    add img, update styles, update data in main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 const app = Vue.createApp({
 	data() {
 		return {
-			product: 'Socks'
+			product: 'Socks',
+			image: '../img/socks_green.jpg',
+			description: 'Socks image',
+			// inStock: true,
+			inventory: 100,
+			onSale: true,
 		}
 	}
 })
